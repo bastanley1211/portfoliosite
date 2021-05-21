5bd1e72abff5b2248ee4e4cd81a5e86e560a6cfc
bastanley1211 <77221444+bastanley1211@users.noreply.github.com>
2021-05-20 20:01:49 -0700

    trying to make form work

--- php/contact_form.php
+++ php/contact_form.php
@@ -1,8 +1,11 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
-
-  echo  $say, ' ', $to;
+$name = $_POST['firstName'];
+$email = $_POST['email'];
+$select = $POST['subject'];
+$formcontent="From: $name \n Message: $select";
+$recipient = "bastanley1211@gmail.com";
+$subject = "bastanley.dev contact form entry";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! I'll get back to you as soon as I can.";
 ?>
